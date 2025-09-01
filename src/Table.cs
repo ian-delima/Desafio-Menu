3a5324ab38a5f9c9e7bed6e644d6f7f26f5fef62
unknown <CONRADO.CANZANI@correo.ucu.edu.uy>
2025-09-01 11:33:53 -0300

    feature-Table

--- src/Table.cs
+++ src/Table.cs
@@ -7,8 +7,24 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Table
     {
+        public int Number { get ; set; }
+        public bool IsOccupied { get; set; }
         private List<Dish> order = new List<Dish>();
 
+        public void Occupy()
+        {
+            IsOccupied = true;
+        }
+
+        public void Free()
+        {
+            IsOccupied = false;
+        }
+
+        public void AddToOrder(Dish)
+        {
+            order.Add(Dish);
+        }
         public bool HasOrders()
         {
             return this.order.Count > 0;
